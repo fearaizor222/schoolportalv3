692bf7869e276394d1140a43116a4ab5e6300951
hoainhanpro <hoainhannro@gmail.com>
2024-06-05 20:14:52 +0700

    Add GIANGVIEN bean

---
+++ src/main/java/bean/GIANGVIEN.java
@@ -0,0 +1,74 @@
+package bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "GIANGVIEN")
+public class GIANGVIEN {
+    @Id
+    private String MAGV;
+    private String MAKHOA;
+    private String HO;
+    private String TEN;
+    private String HOCVI;
+    private String HOCHAM;
+    private String CHUYENMON;
+
+    public String getMAGV() {
+        return MAGV;
+    }
+
+    public void setMAGV(String MAGV) {
+        this.MAGV = MAGV;
+    }
+
+    public String getMAKHOA() {
+        return MAKHOA;
+    }
+
+    public void setMAKHOA(String MAKHOA) {
+        this.MAKHOA = MAKHOA;
+    }
+
+    public String getHO() {
+        return HO;
+    }
+
+    public void setHO(String HO) {
+        this.HO = HO;
+    }
+
+    public String getTEN() {
+        return TEN;
+    }
+
+    public void setTEN(String TEN) {
+        this.TEN = TEN;
+    }
+
+    public String getHOCVI() {
+        return HOCVI;
+    }
+
+    public void setHOCVI(String HOCVI) {
+        this.HOCVI = HOCVI;
+    }
+
+    public String getHOCHAM() {
+        return HOCHAM;
+    }
+
+    public void setHOCHAM(String HOCHAM) {
+        this.HOCHAM = HOCHAM;
+    }
+
+    public String getCHUYENMON() {
+        return CHUYENMON;
+    }
+
+    public void setCHUYENMON(String CHUYENMON) {
+        this.CHUYENMON = CHUYENMON;
+    }
+}
