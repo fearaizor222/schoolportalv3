da417e385403baf19e0044a013616f7717e6423b
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2024-06-09 22:25:26 +0700

    chore: Refactor TeacherSecurityInterceptor for improved session handling

--- src/main/java/interceptor/TeacherSecurityInterceptor.java
+++ src/main/java/interceptor/TeacherSecurityInterceptor.java
@@ -2,13 +2,17 @@ package interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class TeacherSecurityInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (request.getSession().getAttribute("username") == null || !request.getSession().getAttribute("role").equals("teacher") || !request.getSession().getAttribute("role").equals("admin")) {
+        HttpSession session = request.getSession();
+        String role = (String) session.getAttribute("role");
+        String username = (String) session.getAttribute("username");
+        if (username == null || !(role.equals("teacher") || role.equals("admin"))) {
             response.sendRedirect(request.getContextPath() + "/login.htm");
             return false;
         }
